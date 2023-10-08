40f4594e9f0ed64460996bc016d3856281a49042
utpal108 <utpalseu@gmail.com>
2023-10-08 23:21:22 +0600

    Prepare base model completed

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from src.animalDetection import logger
 from animalDetection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from animalDetection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 
 try:
     logger.info(f'>>>>>>> Stage {STAGE_NAME} started <<<<<<<')
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f'>>>>>>> Stage {STAGE_NAME} Completed <<<<<<<\n\nx================================x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = 'Prepare Base Model'
+
+try:
+    logger.info(f'>>>>>>> Stage {STAGE_NAME} started <<<<<<<')
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f'>>>>>>> Stage {STAGE_NAME} Completed <<<<<<<\n\nx================================x')
 except Exception as e:
     logger.exception(e)
