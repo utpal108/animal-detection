3e2867fd8bcf08764e76cc50a0f3149a27000f07
utpal108 <utpalseu@gmail.com>
2023-10-09 23:06:48 +0600

    Prepare callbacks added

--- src/animalDetection/config/configuration.py
+++ src/animalDetection/config/configuration.py
@@ -1,6 +1,7 @@
 from animalDetection.constants import *
 from animalDetection.utils.common import  read_yaml, create_directories
-from animalDetection.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from animalDetection.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig
+import os
 
 # Configuration Manager
 class ConfigurationManager:
@@ -43,3 +44,20 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+    
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
--- src/animalDetection/entity/config_entity.py
+++ src/animalDetection/entity/config_entity.py
@@ -9,6 +9,7 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
+
 @dataclass(frozen =True)
 class PrepareBaseModelConfig:
     root_dir: Path
@@ -20,3 +21,10 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+
+
+@dataclass(frozen=True)
+class PrepareCallbacksConfig:
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: Path
